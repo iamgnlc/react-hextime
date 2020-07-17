b51f235cbcab0f92d78f29b1f29055785ac498f0
GNLC <gianluca.agnocchetti@gmail.com>
2020-07-17 08:27:12 +0100

    updated test

--- src/HexTime/HexTime.test.js
+++ src/HexTime/HexTime.test.js
@@ -2,8 +2,9 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import HexTime from './';
 
-test('renders repo link', () => {
-  render(<HexTime />);
-  const repo = screen.getByText(process.env.REACT_APP_GITHUB_URL);
+test('renders repo link', async () => {
+  const { findByText } = render(<HexTime />);
+
+  const repo = await findByText(process.env.REACT_APP_GITHUB_URL);
   expect(repo).toBeInTheDocument();
 });
